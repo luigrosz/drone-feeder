f1efaaa0a91597508add4827790b99e27d7c5e96
Lui Grosz <luigrosz08@gmail.com>
2022-07-13 18:37:20 -0300

    build: checkstyles

--- src/main/java/com/trybe/dronefeeder/DroneFeederApplication.java
+++ src/main/java/com/trybe/dronefeeder/DroneFeederApplication.java
@@ -6,8 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class DroneFeederApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DroneFeederApplication.class, args);
-	}
+  public static void main(String[] args) {
+    SpringApplication.run(DroneFeederApplication.class, args);
+  }
 
 }
