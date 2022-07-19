512119bd940cc13cb525f3d71430cebf92b82b1b
pedrobrito <pedroab0119@gmail.com>
2022-07-18 23:34:07 -0300

    feat: implemented delivery service on deivelyController

--- src/main/java/com/trybe/dronefeeder/controller/DeliveryController.java
+++ src/main/java/com/trybe/dronefeeder/controller/DeliveryController.java
@@ -1,16 +1,19 @@
 package com.trybe.dronefeeder.controller;
 
 import com.trybe.dronefeeder.dto.DeliveryDto;
-// import com.trybe.dronefeeder.model.DroneModel;
-// import com.trybe.dronefeeder.service.DroneService;
+import com.trybe.dronefeeder.model.DeliveryModel;
+import com.trybe.dronefeeder.service.DeliveryService;
 
-// import java.util.List;
+import java.util.HashMap;
+import java.util.List;
 
-// import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.http.ResponseEntity;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
@@ -22,37 +25,45 @@ import org.springframework.web.bind.annotation.PathVariable;
 @RequestMapping("/delivery")
 public class DeliveryController {
 
-  // @Autowired
-  // private DeliveryController droneService;
+  @Autowired
+  private DeliveryService deliveryService;
 
   /** create controller. */
   @PostMapping()
-  public ResponseEntity<DeliveryDto> create(@RequestBody DeliveryDto delivery) {
-  //   droneService.create(drone);
-    return ResponseEntity.status(HttpStatus.CREATED).body(delivery);
+  // public ResponseEntity<DeliveryDto> create(@RequestBody DeliveryDto delivery) {
+  public ResponseEntity<DeliveryModel> create(@RequestBody DeliveryDto delivery) {
+    DeliveryModel deliveryCreated = deliveryService.create(delivery);
+    return ResponseEntity.status(HttpStatus.CREATED).body(deliveryCreated);
   }
-
+  
   /** getAll controller. */
   @GetMapping()
-  public ResponseEntity<String> findAll() {
-    // public ResponseEntity<List<DroneModel>> findAll() {
-    // List<DroneModel> drones = droneService.findAll();
-    return ResponseEntity.ok("drones");
+  public ResponseEntity<List<DeliveryModel>> findAll() {
+    List<DeliveryModel> delivery = deliveryService.findAll();
+    return ResponseEntity.status(HttpStatus.OK).body(delivery);
   }
 
   /** update controller. */
   @PutMapping("/{id}")
-  public ResponseEntity<String> edit(@RequestBody DeliveryDto delivery, @PathVariable("id") Long id) {
-    // droneService.edit(drone, id);
-    String message = String.format("ID [%d] atualizado", id);
-    return ResponseEntity.ok(message);
+  public ResponseEntity<HashMap<String, String> > edit(@RequestBody DeliveryDto delivery, @PathVariable("id") Long id) {
+    DeliveryModel edited = deliveryService.edit(delivery, id);
+    System.out.println(edited.getId());
+
+    HashMap<String, String> obj = new HashMap<String, String>();
+    String message = String.format("ID [%d] atualizado", edited.getId());
+    obj.put("message", message);
+    return ResponseEntity.status(HttpStatus.OK).body(obj);
   }
 
   /** delete controller. */
   @DeleteMapping("/{id}")
-  public ResponseEntity<String> delete(@PathVariable("id") Long id) {
-    // droneService.delete(id);
-    String message = String.format("ID [%d] removido", id);
-    return ResponseEntity.status(HttpStatus.).body(message);
+  public ResponseEntity<HashMap<String, String>> delete(@PathVariable("id") Long id) {
+    DeliveryModel removed = deliveryService.delete(id);
+    HashMap<String, String> obj = new HashMap<String, String>();
+
+    String message = String.format("ID [%d] removido", removed.getId());
+    obj.put("message", message);
+
+    return ResponseEntity.status(HttpStatus.OK).body(obj);
   }
 } 
