eada84d26bd86d5d2fee2b76aa85f908438cba43
BrenoRCBrito <brenorcbrito@gmail.com>
2022-07-19 20:59:14 -0300

    refactor: make service return DroneDto object :recycle:

--- src/main/java/com/trybe/dronefeeder/service/DroneService.java
+++ src/main/java/com/trybe/dronefeeder/service/DroneService.java
@@ -1,14 +1,13 @@
 package com.trybe.dronefeeder.service;
 
 import com.trybe.dronefeeder.dto.DroneDto;
-import com.trybe.dronefeeder.exceptions.BadRequestException;
 import com.trybe.dronefeeder.exceptions.ResourceNotFoundException;
 import com.trybe.dronefeeder.model.DroneModel;
 import com.trybe.dronefeeder.repository.DroneRepository;
 import com.trybe.dronefeeder.validations.ValidateBody;
 
-import java.util.Date;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -19,56 +18,53 @@ public class DroneService {
   @Autowired
   private DroneRepository droneRepository;
 
+  private DroneDto convertToDto(DroneModel entity) {
+    return new DroneDto(entity);
+  }
+
   /** find all. */
-  public List<DroneModel> findAll() {
-    return droneRepository.findAll();
+  public List<DroneDto> findAll() {
+    return droneRepository.findAll().stream().map(this::convertToDto).collect(Collectors.toList());
   }
 
   /** create. */
-  public DroneModel create(DroneDto drone) {
+  public DroneDto create(DroneDto drone) {
     DroneModel droneModel = new DroneModel();
     ValidateBody.latitude(drone.getLatitude());
     ValidateBody.longitude(drone.getLongitude());
     droneModel.setLatitude(drone.getLatitude());
     droneModel.setLongitude(drone.getLongitude());
-    try {
-      Date date = ValidateBody.date(drone.getLastMaintenance());
-      droneModel.setLastMaintenance(date.toString());
-    } catch (Exception e) {
-      throw new BadRequestException("The date of the request is not valid");
-    }
-    return droneRepository.save(droneModel);
+    String date = ValidateBody.date(drone.getLastMaintenance());
+    droneModel.setLastMaintenance(date);
+
+    return new DroneDto(droneRepository.save(droneModel));
   }
 
   /** find by Id. */
-  public DroneModel findById(Long id) {
-    return droneRepository.findById(id).map(drone -> drone)
+  public DroneDto findById(Long id) {
+    return droneRepository.findById(id).map(this::convertToDto)
         .orElseThrow(() -> new ResourceNotFoundException("No id was found"));
   }
 
   /** update. */
-  public DroneModel edit(DroneDto drone, Long id) {
+  public DroneDto update(DroneDto drone, Long id) {
     return droneRepository.findById(id).map(toUpdate -> {
       ValidateBody.latitude(drone.getLatitude());
       ValidateBody.longitude(drone.getLongitude());
       toUpdate.setLatitude(drone.getLatitude());
       toUpdate.setLongitude(drone.getLongitude());
-      try {
-        Date date = ValidateBody.date(drone.getLastMaintenance());
-        toUpdate.setLastMaintenance(date.toString());
-      } catch (Exception e) {
-        throw new BadRequestException("The date of the request is not valid");
-      }
-      return droneRepository.save(toUpdate);
+      String date = ValidateBody.date(drone.getLastMaintenance());
+      toUpdate.setLastMaintenance(date);
+      return new DroneDto(droneRepository.save(toUpdate));
     }).orElseThrow(() -> new ResourceNotFoundException(
         "Not possible to edit, the provided id does not exist"));
   }
 
   /** delete. */
-  public DroneModel delete(Long id) {
+  public DroneDto delete(Long id) {
     return droneRepository.findById(id).map(toDelete -> {
       droneRepository.deleteById(id);
-      return toDelete;
+      return new DroneDto(toDelete);
     }).orElseThrow(
         () -> new ResourceNotFoundException(
             "Not possible to delete, the provided id does not exist"));
