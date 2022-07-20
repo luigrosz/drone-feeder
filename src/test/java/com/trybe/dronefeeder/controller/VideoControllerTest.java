885db3bf3842b0b454fb813f37c0907cb6b712e3
Lui Grosz <luigrosz08@gmail.com>
2022-07-20 14:37:13 -0300

    test: videoController getAll router

---
+++ src/test/java/com/trybe/dronefeeder/controller/VideoControllerTest.java
@@ -0,0 +1,40 @@
+package com.trybe.dronefeeder.controller;
+
+import static org.hamcrest.Matchers.containsString;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.delete;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.put;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.trybe.dronefeeder.service.VideoService;
+
+import org.junit.jupiter.api.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.servlet.MockMvc;
+
+@RunWith(SpringRunner.class)
+@WebMvcTest(VideoController.class)
+@AutoConfigureMockMvc
+public class VideoControllerTest {
+  @Autowired
+  private MockMvc mockMvc;
+
+  @MockBean
+  private VideoService videoService;
+
+  @Test
+  public void getAllRoute() throws Exception {
+    mockMvc.perform(get("/video"))
+        .andExpect(status().isOk())
+        .andExpect(content().string(containsString("[]")));
+  }
+}
